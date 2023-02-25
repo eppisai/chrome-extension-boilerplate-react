b6b6618e83550bf71aa94c1ed74cc6aa61bd7aee
Aman <54789531+eppisai@users.noreply.github.com>
2023-02-26 00:17:01 +0530

    CURRENT CHANGES

--- src/pages/Content/index.js
+++ src/pages/Content/index.js
@@ -1,6 +1,68 @@
-import { printLine } from './modules/print';
+let selectedPTag = null;
 
-console.log('Content script works!');
-console.log('Must reload extension for modifications to take effect.');
+const popupElement = document.createElement('div');
+popupElement.style.position = 'absolute';
+popupElement.style.top = '0';
+popupElement.style.right = '0';
+popupElement.style.width = '200px';
+popupElement.style.height = '200px';
+popupElement.style.backgroundColor = 'white';
+popupElement.style.display = 'none';
+popupElement.style.zIndex = '2147483647';
 
-printLine("Using the 'printLine' function from the Print Module");
+const formElement = document.createElement('form');
+formElement.style.padding = '10px';
+
+const inputElement = document.createElement('input');
+inputElement.setAttribute('type', 'text');
+inputElement.setAttribute('placeholder', 'Enter your name');
+inputElement.style.width = '100%';
+inputElement.style.marginBottom = '10px';
+
+const buttonElement = document.createElement('button');
+buttonElement.textContent = 'Submit';
+buttonElement.style.width = '100%';
+
+formElement.appendChild(inputElement);
+formElement.appendChild(buttonElement);
+
+popupElement.appendChild(formElement);
+
+document.body.appendChild(popupElement);
+
+function handlePTagClick(event) {
+  const pTags = document.querySelectorAll('.ql-editor p');
+
+  if (event.target.matches('.ql-editor p')) {
+    const clickedPTag = event.target;
+
+    if (selectedPTag !== null) {
+      selectedPTag.style.backgroundColor = '';
+    }
+
+    clickedPTag.style.backgroundColor = 'yellow';
+
+    const boundingRect = clickedPTag.getBoundingClientRect();
+    popupElement.style.top = boundingRect.top + 'px';
+    popupElement.style.left = boundingRect.right + '10px';
+
+    popupElement.style.display = 'block';
+
+    selectedPTag = clickedPTag;
+  } else {
+    popupElement.style.display = 'none';
+    selectedPTag = null;
+  }
+}
+
+document.addEventListener('click', handlePTagClick);
+
+buttonElement.addEventListener('click', function(event) {
+  event.preventDefault();
+
+  const name = inputElement.value;
+
+  console.log(`Hello, ${name}!`);
+
+  popupElement.style.display = 'none';
+});
--- src/pages/Popup/Popup.jsx
+++ src/pages/Popup/Popup.jsx
@@ -1,25 +1,22 @@
-import React from 'react';
-import logo from '../../assets/img/logo.svg';
-import Greetings from '../../containers/Greetings/Greetings';
+import React, {useEffect, useState} from 'react';
 import './Popup.css';
 
 const Popup = () => {
+  const [apiKey, setApiKey] = useState('');
+  const [r,setR] = useState(0)
+  console.log(apiKey)
+  useEffect(() => {
+    chrome.storage.local.get(['apiKey']).then(({apiKey}) => {
+      setApiKey(apiKey)
+    });
+  },[])
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/pages/Popup/Popup.jsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React!
-        </a>
-      </header>
+    <div>
+       extension popup {apiKey}
+       <button onClick={() => {
+        chrome.storage.local.set({apiKey: 'aman'})
+        setR('d');
+       }}>Reload</button>
     </div>
   );
 };
